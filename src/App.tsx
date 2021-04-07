92a1bc9ad7fa3e5236da237f687728baf0a12f61
Keven Andrade <hirlandeer7@gmail.com>
2021-04-07 15:13:21 -0100

    estrutura de prog

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
-function App() {
-  return (
-   <h1>Git_repositories</h1>
-  );
-}
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+    <>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+        <GlobalStyle />
+    </>
+);
 
 export default App;
