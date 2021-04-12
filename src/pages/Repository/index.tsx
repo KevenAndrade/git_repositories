c90f7a88643025346ef8057d7d3fa9005783a5cb
Keven Andrade <hirlandeer7@gmail.com>
2021-04-12 16:53:03 -0100

    ui de repositorios

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft } from 'react-icons/fi';
 
-import {} from './style';
+import { Header, RepositoryInfo } from './style';
+import logo from '../../assets/github_logo.svg';
 
 interface RepositoryParams {
     repository: string;
@@ -9,7 +11,44 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository: {params.repository}</h1>;
+    return (
+        <>
+            <Header>
+                <img src={logo} alt="Gitfinder" style={{ width: 50 }} />
+                <Link to="/dashboard">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img
+                        src="https://avatars.githubusercontent.com/u/65917547?v=4"
+                        alt="perfil"
+                    />
+                    <div>
+                        <strong>KevenAndrade/API_NPS</strong>
+                        <p>Its a NPS !! Did you ever recieve</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1230</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>78</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>55</strong>
+                        <span>Issues abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+        </>
+    );
 };
 
 export default Repository;
--- src/pages/Repository/style.ts
+++ src/pages/Repository/style.ts
@@ -1,1 +1,86 @@
 import styled, { css } from 'styled-components';
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    a {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        color: #a8a8b3;
+        transition: color 0.2s;
+
+        &:hover {
+            color: #666;
+        }
+
+        svg {
+            margin-right: 4px;
+        }
+    }
+`;
+
+export const RepositoryInfo = styled.section`
+    margin-top: 80px;
+
+    header {
+        display: flex;
+        align-items: center;
+    }
+
+    img {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+    }
+
+    div {
+        margin-left: 24px;
+
+        strong {
+            font-size: 36px;
+            color: #edeaea;
+        }
+
+        p {
+            font-size: 18px;
+            color: #edeaea;
+            margin-top: 4px;
+        }
+    }
+
+    ul {
+        display: flex;
+        list-style: none;
+        margin-top: 40px;
+
+        li {
+            & + li {
+                margin-left: 40px;
+            }
+
+            strong {
+                display: block;
+                font-size: 36px;
+                color: #edeaea;
+            }
+
+            span {
+                display: block;
+                color: #edeaea;
+                margin-top: 4px;
+            }
+        }
+    }
+`;
+
+export const Title = styled.h1`
+    font-size: 45px;
+    color: #fff;
+
+    max-width: 450px;
+    line-height: 56px;
+    margin-top: 30px;
+`;
